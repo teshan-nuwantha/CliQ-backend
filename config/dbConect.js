5c451ddf141b98266a47f22710267bf69534cd4a
teshan-nuwantha <115331768+teshan-nuwantha@users.noreply.github.com>
2023-04-26 21:28:43 +0530

    add DB link

--- config/dbConect.js
+++ config/dbConect.js
@@ -2,6 +2,6 @@ const { default: mongoose } = require("mongoose")
 
 const dbConnect = () =>{
     try{
-        const conn= mongoose.connect("");//link
+        const conn= mongoose.connect("mongodb+srv://admin:admin@cliq.eazhhvq.mongodb.net/cliQ?retryWrites=true&w=majority");//link
     }
 }

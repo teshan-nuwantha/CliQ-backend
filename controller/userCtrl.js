554071c4a0863235f99b2a1c2d95287869836e88
Udara Madusanka <udaramadu19980823@gmail.com>
2023-04-26 23:54:30 +0530

    user momdel and userCtrl

---
+++ controller/userCtrl.js
@@ -0,0 +1,20 @@
+const user = require("../models/userModel");
+
+const createUser = async (req,res) => {
+    const email = req.body.email;
+    const findUser = await user.findOne(email);
+
+    if(!findUser){
+        //Create a new user
+        const newUser = User.creste(req.body);
+        res.json(newUser);
+    }
+    else{
+        res.json(
+            {
+                msg: "User alredy Exits",
+                success: false,
+            }
+        )
+    }
+};

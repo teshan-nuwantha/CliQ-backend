a70c3162308044ca8d0cc214dcfe0d19e3e81da8
Udara Madusanka <udaramadu19980823@gmail.com>
2023-04-26 23:28:01 +0530

    database connection

--- config/dbConect.js
+++ config/dbConect.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose")
 
 const dbConnect = () =>{
     try{
-        const conn= mongoose.connect("mongodb+srv://admin:admin@cliq.eazhhvq.mongodb.net/cliQ?retryWrites=true&w=majority");//link
+        const conn= mongoose.connect(process.env.MONGODB_URL );
     }
     catch(error){
         console.log("Database error");
